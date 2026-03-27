scylladb/nodejs-rs-driver

Custom async napi bridging
This commit adds a custom async bridge between Rust and JavaScript using N-API,
allowing for scheduling of async tasks without the use of tokio::spawn,
that is used when creating async functions through the napi-rs macros.

The main motivation for this change, was to improve the performance of the driver.
With the existing approach we spend a lot of CPU time on synchronization
between the main thread and the tokio threads. By reducing CPU time,
I aim to also improve the driver runtime.

This approaches pools all the futures on the Node.js main thread,
replacing napi-rs's built-in async task system which polls on
Tokio worker threads.

Architecture:
- Single weak ThreadsafeFunction (TSFN) shared across all futures,
  with manual ref/unref to control Node.js event loop lifetime
- FutureRegistry (thread-local on main thread) stores in-flight futures
  paired with their napi_deferred handles
- Per-future Waker backed by Arc<WakerInner> implementing the Wake trait,
  which pushes the future id into a shared woken_ids vec and signals
  the TSFN
- Coalesced signaling via AtomicBool prevents flooding the event loop
  when multiple wakers fire simultaneously
- Single-threaded Tokio runtime drives the I/O reactor only; futures
  are polled on the main thread inside the TSFN callback with the
  Tokio runtime context entered

Key design decisions:
- Polling on main thread ensures napi_env is always valid during
  ToNapiValue conversion, avoiding cross-thread napi safety issues
- Type-erased BoxFuture and SettleCallback allow heterogeneous futures
  in a single HashMap without leaking generic parameters
- Promise created via raw napi_create_promise/napi_resolve_deferred
  to bypass napi-rs's async machinery entirely

[This commit including this commit message was created with heavy use of LLM tools.
At the current moment, the code was slightly refactored to partially match
the existing style kept at this repository.]

src/lib.rs
@@ -12,3 +12,4 @@ pub mod session;
 pub mod tests;
 pub mod types;
 pub mod utils;
+pub mod casync;

adespawn/integration-hell

src/lib.rs
@@ -2,6 +2,7 @@
 extern crate napi_derive;
 
 // Link other files
+pub mod async_bridge;
 pub mod errors;
 pub mod metadata;
 pub mod options;